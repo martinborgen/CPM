19153e0872817d57e999cc94ab6c704c92a3a9cc
Martin Borgén <martinb.nymail@gmail.com>
2023-06-03 23:37:24 +0200

    started on a GUI

---
+++ critical_path_GUI.py
@@ -0,0 +1,19 @@
+# Critical path program using TKinter for GUI
+# Started 2023-04-29
+# Martin Borgén
+
+import tkinter as tk
+
+class Main():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Critical Path")
+
+
+
+def main():
+    main = Main()
+    tk.mainloop()
+
+if __name__ == '__main__':
+    main()
