64b5e2103321e8de31ad898b2c60a16c3bd26b60
Martin Borgén <martinb.nymail@gmail.com>
2023-08-26 14:10:16 +0200

    Implemented the possibility for the user to choose the csv separator, with suitable error messages

--- csv_module.py
+++ csv_module.py
@@ -2,13 +2,22 @@
 # Martin Borgén
 # 2023-02-19
 
+from exceptions import CSVseparatorError
+
 def csv_reader(filename: str, sep=',', depSep=','):
     # assume task,dur,dep -format for now. Dep is N ammounts of dependencies
+    if (len(sep) == 0):
+        raise CSVseparatorError
+
     file = open(filename)
     lines = file.readlines()
     out = []
     for line in lines:
         splitted = line.split(sep, 2)
+
+        if len(splitted) != 3:
+            raise CSVseparatorError
+
         newLine = []
         newLine += splitted[0:2]
         deps = splitted[2]
---
+++ exceptions.py
@@ -0,0 +1,2 @@
+class CSVseparatorError(Exception):
+    pass
